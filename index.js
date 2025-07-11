0d01b4538e5cbb30fa375456f1a925d7d0ec86ea
antonlesnikov <166492919+js-hunter@users.noreply.github.com>
2025-07-11 18:20:22 +0500

    Create index.js
    Просто накидал пример для визуальной шпаргалки

---
+++ index.js
@@ -0,0 +1,13 @@
+export default clas formValidation {
+  static rules = getMapFromObj({...})
+
+  constructor() {...}
+
+  static getActionCfg(modal) {...}
+
+  static getParams(handler) {...}
+
+  static handleModalConfirm(instance, event) {...}
+
+  static handleModalCancel(instance, event) {...}
+}
